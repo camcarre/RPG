a4119bdbd8d760f0d2177aa24cf9a4800e60bd6d
dcamille <camilledouaud@ynov.Com>
2024-03-25 11:06:03 +0100

    objet

---
+++ objet/Ether.ts
@@ -0,0 +1,13 @@
+class Ether {
+    private readonly magicPointsRestored: number;
+
+    constructor() {
+        this.magicPointsRestored = 0.3; // 30% of magic points restored
+    }
+
+    use() {
+        // Logic to restore magic points
+    }
+}
+
+export default Ether;
