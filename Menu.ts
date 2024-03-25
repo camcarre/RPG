5bcd87b767865e8b542fd7e026797f898c592019
theo.delporte <theo.delporte@ynov.com>
2024-03-25 08:45:05 +0100

    ajout menu et rajout class caractère

--- Menu.ts
+++ Menu.ts
@@ -1,9 +1,38 @@
-class Menu{
+class Menu {
     question: string;
     options: string[];
-
-    constructor(question : string, options : string){
+    
+    constructor(question: string, options: string[]) {
         this.question = question;
         this.options = options;
     }
+
+    askQuestion(): string {
+        console.log(this.question);
+        this.options.forEach((option, index) => {
+            console.log(`${index + 1}. ${option}`);
+        });
+
+        
+        return this.getUserChoice();
+    }
+
+    
+    private getUserChoice(): string {
+        const userInput = prompt("Votre choix: ");
+        const choice = parseInt(userInput);
+        if (!isNaN(choice) && choice >= 1 && choice <= this.options.length) {
+            return this.options[choice - 1];
+        } else {
+            console.log("Choix invalide. Veuillez entrer un numéro correspondant à une option.");
+            return this.getUserChoice();
+        }
+    }
 }
+
+const question = "Quelle est votre couleur préférée ?";
+const options = ["Rouge", "Bleu", "Vert", "Jaune"];
+const menu = new Menu(question, options);
+const response = menu.askQuestion();
+console.log(`Vous avez choisi: ${response}`);
+
