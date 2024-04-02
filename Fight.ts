5f03d06506ed3bce01063a6fa6ee16e986458747
benphihe <150243312+benphihe@users.noreply.github.com>
2024-04-02 08:06:39 +0200

    add the base of the fight

--- Fight.ts
+++ Fight.ts
@@ -14,6 +14,41 @@ class Fight {
         this.team1 = team1;
         this.team2 = team2;
     }
+    for (let i = 0; i < this.order.length; i++) {
+        const character = this.order[i];
+        if (character.pvcurrent > 0) {
+          console.log(`${character.name}'s turn`);
+          const choice = menu.displayAndGetChoice();
+          switch (choice) {
+            case 1:
+              this.attack(character, this.team2);
+              break;
+            case 2:
+              this.specialAttack(character, this.team2);
+              break;
+            case 3:
+              this.heal(character, this.team1);
+              break;
+            case 4:
+              this.displayTeam(this.team1);
+              break;
+            default:
+              break;
+          }
+          if (this.team2.length === 0) {
+            console.log("Team 1 wins!");
+            break;
+          }
+          if (this.team1.length === 0) {
+            console.log("Team 2 wins!");
+            break;
+          }
+        }
+      }
+    characterDeath(character: Character, team: Character[]) {
+        const index = team.indexOf(character);
+        team.splice(index, 1);
+      }
 
     determineTurnOrder(): Character[] {
         const allCharacters = this.team1.concat(this.team2);
