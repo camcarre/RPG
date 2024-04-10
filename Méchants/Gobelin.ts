9ea9ad8999c87a0b1cc8a8b89a4e0e29f248affb
theo.delporte <theo.delporte@ynov.com>
2024-04-10 08:10:41 +0200

    début action item

--- Méchants/Gobelin.ts
+++ Méchants/Gobelin.ts
@@ -2,6 +2,6 @@ import Character from '../Character.ts';
 
 export default class Gobelin extends Character {
     constructor() {
-        super("Gobelin", 20, 10, 10, 50, 50);
+        super("Gobelin", 20, 80, 10, 50, 50);
     }
 }
--- objet/Ether.ts
+++ objet/Ether.ts
@@ -1,13 +1,10 @@
-class Ether {
-    private readonly magicPointsRestored: number;
+import Character from '../Character';
+import Objet from '../Objet';
 
-    constructor() {
-        this.magicPointsRestored = 0.3;
-    }
-
-    use() {
-       
+export default class Ether extends Objet {
+    utiliser(cible: Character): void {
+        const magieRestores = Math.ceil(cible.magieMax * 0.3);
+        cible.magieCurrent = Math.min(cible.magieCurrent + magieRestores, cible.magieMax);
+        console.log(`${cible.name} récupère ${magieRestores} points de magie grâce à l'ether.`);
     }
 }
-
-export default Ether;
