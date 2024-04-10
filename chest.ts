95f14e3c0a84a09df8a609ee170cdf151b213d6d
benphihe <150243312+benphihe@users.noreply.github.com>
2024-04-10 14:22:51 +0200

    finalisation de chest.ts

--- chest.ts
+++ chest.ts
@@ -15,27 +15,39 @@ class Chest {
             this._objet = objet;
             this._ouvert = false;
     }
-    getRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-        
-      }
+    getRandomNumber(min: number, max: number) {
+    
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
     open(currentPlayer: Character) {
       const randomNumber = this.getRandomNumber(1, 2);
+
+    
+      
       if (randomNumber === 1) {
         if (obj.length > 0) {
             for (let i = 0; i < nbObj; i++) {
                 const randomIndex = Math.floor(Math.random() * obj.length);
                     const objInChest = obj[randomIndex];
-                } 
-    }
-        console.log('Vous avez trouvé ' + chestContents + 'dans le coffre');
+                    
+            } 
+        }
+        
+        console.log('Vous avez trouvé deux objets dans le coffre');
 
-      }
-        else {
+      } else {
             console.log('Le coffre était piégé, vous avez perdu 10 points de vie');
             currentPlayer.pvcurrent -= 10;
         }
     }
-    }
+  }
+
+  const chest = new Chest(new obj[0]);
+  const character = new Character('Toto', 10, 5, 5, 100, 100)
+
+  const test = chest.getRandomNumber(20, 30)
+
+  chest.open(character)
+
 
