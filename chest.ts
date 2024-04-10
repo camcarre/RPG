a9b692a0e46d4f44d04386517b76012f277cc4f6
benphihe <150243312+benphihe@users.noreply.github.com>
2024-04-10 11:04:28 +0200

    modification de chest.ts

--- chest.ts
+++ chest.ts
@@ -1,40 +1,41 @@
-import DemiEtoile from "./objet/demietoile";
-import Ether from "./objet/Ether";
-import MorceauEtoile from "./objet/morceauetoile";
-import potion from "./objet/potion";
-import Character from "./Character";
+import demietoile from "./objet/demietoile.ts";
+import Ether from "./objet/Ether.ts";
+import MorceauEtoile from "./objet/MorceauEtoile.ts";
+import potion from "./objet/potion.ts";
+import Character from "./Character.ts";
 
-const obj = [DemiEtoile, Ether, MorceauEtoile, potion];
+const obj = [demietoile, Ether, MorceauEtoile, potion];
 const nbObj = 2;
-const objInChest = [];
+const chestContents = [];
 class Chest {
-    private _objet: potion | DemiEtoile | MorceauEtoile | Ether;
+    private _objet: potion | demietoile | MorceauEtoile | Ether;
     private _ouvert: boolean;
 
-    constructor(objet: potion | DemiEtoile | MorceauEtoile | Ether) {
-        this._objet = objet;
-        this._ouvert = false;
+    constructor(objet: potion | demietoile | MorceauEtoile | Ether) {
+            this._objet = objet;
+            this._ouvert = false;
     }
-    if (obj.length > 0) {
-        for (let i = 0; i < nbObj; i++) {
-          const randomIndex = Math.floor(Math.random() * obj.length);
-            const objInChest = obj[randomIndex];
-          } 
-    }
-}
-    
     getRandomNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
+        
       }
-      
-      const randomNumber = this.getRandomNumber(1, 2);
 
+    open(currentPlayer: Character) {
+      const randomNumber = this.getRandomNumber(1, 2);
       if (randomNumber === 1) {
-        console.log('Vous avez trouvé ' + objInChest + 'dans le coffre');
+        if (obj.length > 0) {
+            for (let i = 0; i < nbObj; i++) {
+                const randomIndex = Math.floor(Math.random() * obj.length);
+                    const objInChest = obj[randomIndex];
+                } 
+    }
+        console.log('Vous avez trouvé ' + chestContents + 'dans le coffre');
 
       }
         else {
             console.log('Le coffre était piégé, vous avez perdu 10 points de vie');
             currentPlayer.pvcurrent -= 10;
         }
-      
+    }
+    }
+
